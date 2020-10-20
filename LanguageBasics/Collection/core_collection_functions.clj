63a6598b17c77d3814cb836c4252ee1d4633ab48
Ankush Chavan <cankush625@gmail.com>
2020-10-21 01:56:44 +0530

    Clojure Core functions like conj, assoc, dissoc and merger are implemented
    Signed-off-by: Ankush Chavan <cankush625@gmail.com>

--- LanguageBasics/Collection/core_collection_functions.clj
+++ LanguageBasics/Collection/core_collection_functions.clj
@@ -29,3 +29,48 @@
 ; contains function should not be used with the lists because lists are not keyed sequenses
 
 ; (println(contains? '(1 7 2 5) 2)) ;(IllegalArgumentException)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+; core collection functions
+(println ";;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;" )
+
+; conj: 'conjoin' returns new collection with an added element or key-value pair
+
+(println(conj {1 2 "hi" 4} ["bye" 2])) ; output: {1 2, hi 4, bye 2}
+(println(conj {1 2 "hi" 5} ["hi", 3])) ; output: {1 2, hi 3}
+
+(println(conj [1 2 4 6 7] true)) ; output: [1 2 4 6 7 true]
+(println(conj '(1 2 4 6 7) true)) ; output: (true 1 2 4 6 7)
+
+; assoc: 'associate' returns new collection with added/modified value for given key or index
+
+(println ";;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;" )
+
+(println(assoc {1 2 "hi" 4} "bye" 2)) ; output: {1 2, hi 4, bye 2}
+(println(assoc {1 2 "hi" 5} "hi", 3)) ; output: {1 2, hi 3}
+
+(println(assoc [1 2 4 6 7] 3 true)) ; output: [1 2 4 true 7]
+(println(assoc [1 2 4 6 7] 4 true)) ; output: [1 2 4 6 true]
+(println(assoc [1 2 4 6 7] 5 true)) ; output: [1 2 4 6 7 true]
+
+; (println(assoc '(1 2 4 6 7) 3 true)) ; output: throws error(assoc doesn't work with list)
+
+; dissoc: 'dissociate' returns new map in which a key has been removed
+
+(println ";;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;" )
+
+(println(dissoc {1 2 "hi" 4} "hi")) ; output: {1 2}
+(println(dissoc {1 2 "hi" 4} "bye")) ; output: {1 2, hi 4}
+
+; (println(dissoc [1 2 4 6 7] 3)) ; output: throws exception(dissoc doesn't work on vectors)
+; (println(dissoc '(1 2 4 6 7) 3)) ; output: throws exception(dissoc doesn't work on lists)
+
+; merge: returns a new map that combines the key-value pairs of one or more maps
+
+(println ";;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;" )
+
+(println(merge {1 2 "hi" 4})) ; output: {1 2, hi 4}
+(println(merge {1 2 "hi" 4} {"hi" 5, :yo 4})) ; output: {1 2, hi 5, :yo 4}
+(println(merge {1 2 "hi" 4} {"hi" 5, :yo 4} {:yo 6})) ; output: {1 2, hi 5, :yo 6}
